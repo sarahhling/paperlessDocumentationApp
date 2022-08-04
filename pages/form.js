bd0d79e2a37449433adc321ea9293cf696880e7c
hnadrian <hngt.adrian@gmail.com>
2022-08-04 02:27:14 -0700

    Add form.js

---
+++ pages/form.js
@@ -0,0 +1,12 @@
+import {useForm} from 'react-hook-form';
+
+
+function Validate() {
+    const {register, handleSubmit, errors, reset} = useForm();
+    return (
+
+    );
+
+}
+
+export default Validate;
