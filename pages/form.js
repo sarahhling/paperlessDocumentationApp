dee56bed79877242246770377581552a9b73a3ec
hnadrian <hngt.adrian@gmail.com>
2022-08-04 15:42:28 -0700

    supabase import

--- pages/form.js
+++ pages/form.js
@@ -1,16 +1,21 @@
+import React from 'react';
 import {useForm} from 'react-hook-form';
+import { supabase } from "../utils/supabaseClient.js";
 
+
+  
 
 function Validate() {
     const {register, handleSubmit, errors, reset} = useForm();
     return (
        <form>
             <label for="name">Product Name</label>
-            <input type="text" name="name" {...register('test', { required: true })}/>
+            <input type="text" id="name" name="name" {...register('productName', { required: true, maxLength: 100 })}/>
             <label for="price">Price</label>
-            <input type="text" name="price" {...register('test', { required: true })}/>
+            <input type="number" id= "price" name="price" {...register('productPrice', { required: true })}/>
             <label for="quantity">Quantity</label>
-            <input type="number" name="quantity" {...register('test', { required: true })}/>
+            <input type="number" id="quanity" name="quantity" {...register('productQuantity', { required: true })}/>
+            <button type="submit">Submit</button>
        </form>
     );
 
