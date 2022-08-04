8eb44a43468cad3287ef1abc1d82a6de3c09aca0
hnadrian <hngt.adrian@gmail.com>
2022-08-04 02:37:07 -0700

    Add some form fields

--- pages/form.js
+++ pages/form.js
@@ -4,7 +4,14 @@ import {useForm} from 'react-hook-form';
 function Validate() {
     const {register, handleSubmit, errors, reset} = useForm();
     return (
-        <div></div>
+       <form>
+            <label for="name">Product Name</label>
+            <input type="text" name="name" ref={register({required: true})}/>
+            <label for="price">Price</label>
+            <input type="text" name="price" ref={register({required: true})}/>
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity" ref={register({required: true})}/>
+       </form>
     );
 
 }
