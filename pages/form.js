e4d5e38aa5a5cd562b4dbb9711729e523da9dcd9
hnadrian <hngt.adrian@gmail.com>
2022-08-07 15:40:39 -0700

    Add submission to supabase

--- pages/form.js
+++ pages/form.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import {useForm} from 'react-hook-form';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { supabase } from "../utils/supabaseClient.js";
 
-
-  
-
-function Validate() {
+function Form() {
     const {register, handleSubmit, errors, reset} = useForm();
+    // const onSubmit = (data) => {
+    //   console.log(data);
+    // }
+    const onSubmit = async (data) => {
+      console.log(data);
+      await supabase.from('Items').insert([data]);
+    }
+
     return (
-       <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
             <label for="name">Product Name</label>
-            <input type="text" id="name" name="name" {...register('productName', { required: true, maxLength: 100 })}/>
+            <input type="text" id="name" name="name" {...register('name', { required: true })} />
             <label for="price">Price</label>
-            <input type="number" id= "price" name="price" {...register('productPrice', { required: true })}/>
+            <input type="number" id= "price" name="price"  {...register('price', { required: true })} />
             <label for="quantity">Quantity</label>
-            <input type="number" id="quanity" name="quantity" {...register('productQuantity', { required: true })}/>
+            <input type="number" id="quanity" name="quantity" {...register('quantity', { required: true })} />
             <button type="submit">Submit</button>
        </form>
     );
 
 }
 
-export default Validate;
+export default Form;
