716bf69cfd414ea6071e22892fe8405338e8712c
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-08-08 01:52:03 -0700

    fixed logo padding

--- components/logo.js
+++ components/logo.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Logo({ pic }) {
   return (
-    <div className="text-center">
+    <div className="text-center pt-5">
       <Link href="/">
         <Image src={pic} width={100} height={100} />
       </Link>
