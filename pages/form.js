d5c42c817d87e0e54141329fb674037093e70e25
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-08-11 14:48:13 -0700

    fix syntax

--- pages/form.js
+++ pages/form.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React from "react";
 import { useSession } from "next-auth/react";
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 import { supabase } from "../utils/supabaseClient.js";
 
 function Form() {
-    const { data: session } = useSession();
-    
-    const current_user = session.user.username;
-    console.log(current_user);
+  const { data: session } = useSession();
 
-    const {register, handleSubmit, errors, reset} = useForm();
-    const onSubmit = async (data) => {
-      data['user'] = current_user;
-      console.log(data);
-      await supabase.from('Items').insert([data]);
-    }
- 
-    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-            <label for="name">Product Name</label>
-            <input type="text" id="name" name="name" {...register('name', { required: true })} />
-            <label for="price">Price</label>
-            <input type="number" id= "price" name="price"  {...register('price', { required: true })} />
-            <label for="quantity">Quantity</label>
-            <input type="number" id="quanity" name="quantity" {...register('quantity', { required: true })} />
-            <button type="submit">Submit</button>
-       </form>
-    );
+  const current_user = session.user.username;
+  console.log(current_user);
 
+  const { register, handleSubmit, errors, reset } = useForm();
+  const onSubmit = async (data) => {
+    data["user"] = current_user;
+    console.log(data);
+    await supabase.from("Items").insert([data]);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="name">Product Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        {...register("name", { required: true })}
+      />
+      <label htmlFor="price">Price</label>
+      <input
+        type="number"
+        id="price"
+        name="price"
+        {...register("price", { required: true })}
+      />
+      <label htmlFor="quantity">Quantity</label>
+      <input
+        type="number"
+        id="quanity"
+        name="quantity"
+        {...register("quantity", { required: true })}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Form;
