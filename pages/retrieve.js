0acaa11f1af88f9c6b867be6f217909b44fdf45f
wicaksa <wicaksa_r@yahoo.com>
2022-08-12 20:51:50 -0700

    Added logic to retrieve data from db for corresponding user and display it in a table.

--- pages/retrieve.js
+++ pages/retrieve.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSession } from "next-auth/react";
 import { supabase } from "../utils/supabaseClient.js";
+import styles from "../styles/Retrieve.module.css";
 
 function RetrievePage() {
+
     //Checks for permission to access the page
     const { data: session } = useSession();
     const user = session.user.username;
 
+    const [posts, setPosts] = useState([]);
+
+    useEffect( () => {
+        fetchdata()
+    }, [])
+
     async function fetchdata() {
+        // SQL Select items that the user inputted 
         const { data, error } = await supabase
         .from('Items')
         .select()
+        .eq('user', user)
         console.log(data)
-    }
-
-    if (session.user.username == data.username) {
-        //Fetches data from the database
+        setPosts(data);
     }
 
     return (
-        <button onClick={fetchdata}>Retrieve Data</button>
+        <div className="App">
+            <table className={styles.retrievetable}>
+                <th className={styles.retrieveth}>Item</th>
+                <th className={styles.retrieveth}>Price</th>
+                <th className={styles.retrieveth}>Quantity</th>
+            </table>
+            
+            {posts.map(post => (
+                <div key="{post.id}">
+                    <table className={styles.retrievetable}>
+                        <tr className={styles.itemRow}>
+                            <td className={styles.retrieveth}>{post.name}</td>
+                            <td className={styles.retrieveth}> {post.price}</td>
+                            <td className={styles.retrieveth}>{post.quantity}</td>
+                        </tr>
+                    </table>
+                </div>
+            ))}
+        </div>
     );
 }
 
