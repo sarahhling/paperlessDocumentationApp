d7bb11b6c948361c5c3c7e17e0c54ce9e798ea69
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-08-12 18:21:27 -0700

    update retrieve style, add loading page

--- pages/retrieve.js
+++ pages/retrieve.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React from "react";
 import { useSession } from "next-auth/react";
 import { supabase } from "../utils/supabaseClient.js";
 
-function RetrievePage() {
-    //Checks for permission to access the page
-    const { data: session } = useSession();
-    const user = session.user.username;
+export default function Retrieve() {
+  const { data: session, status } = useSession();
 
-    async function fetchdata() {
-        const { data, error } = await supabase
-        .from('Items')
-        .select()
-        console.log(data)
-    }
-
-    if (session.user.username == data.username) {
-        //Fetches data from the database
-    }
-
-    return (
-        <button onClick={fetchdata}>Retrieve Data</button>
-    );
+  return status === "authenticated" ? RetrievePage(session) : LoadingPage();
 }
 
-export default RetrievePage;
+function RetrievePage(session) {
+  //Checks for permission to access the page
+  const user = session.user.username;
+
+  async function fetchdata() {
+    const { data, error } = await supabase.from("Items").select();
+    console.log(data);
+  }
+
+  //   if (session.user.username == data.username) {
+  //     //Fetches data from the database
+  //   }
+
+  return (
+    <div className="col-md-12 text-center pt-3">
+      <button
+        className="btn btn-outline-info"
+        type="button"
+        onClick={fetchdata}
+      >
+        Retrieve Data
+      </button>
+    </div>
+  );
+}
+
+function LoadingPage() {
+  return <></>;
+}
