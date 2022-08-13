f24cdd1ce5ba1f475d7c9162e0715e494f7f8a1e
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-08-12 18:10:28 -0700

    added alternate form page

--- pages/form.js
+++ pages/form.js
@@ -2,37 +2,50 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
 import { supabase } from "../utils/supabaseClient.js";
+import { useState, useEffect } from "react";
 
-function Form() {
-  const { data: session } = useSession();
+export default function Form() {
+  const { data: session, status } = useSession();
+  const { register, handleSubmit, errors, reset } = useForm();
+  const [username, setUsername] = useState();
 
-  const current_user = session.user.username;
+  useEffect(() => {
+    setUsername(session?.user?.username);
+  }, [session]);
+
+  return status === "authenticated"
+    ? FormPage(username, register)
+    : LoadingPage();
+}
+
+function FormPage(username, register) {
+  const current_user = username;
   console.log(current_user);
 
-  const { register, handleSubmit, errors, reset } = useForm();
-  const onSubmit = async (data) => {
-    data["user"] = current_user;
-    console.log(data);
-    await supabase.from("Items").insert([data]);
-  };
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label for="name">Product Name</label>
+    <form
+      onSubmit={async (data) => {
+        data["user"] = current_user;
+        console.log(data);
+        await supabase.from("Items").insert(data);
+      }}
+    >
+      <label htmlFor="name">Product Name</label>
       <input
         type="text"
         id="name"
         name="name"
         {...register("name", { required: true })}
       />
-      <label for="price">Price</label>
+      <label htmlFor="price">Price</label>
       <input
         type="number"
         id="price"
         name="price"
+        step="0.01"
         {...register("price", { required: true })}
       />
-      <label for="quantity">Quantity</label>
+      <label htmlFor="quantity">Quantity</label>
       <input
         type="number"
         id="quanity"
@@ -44,4 +57,6 @@ function Form() {
   );
 }
 
-export default Form;
+function LoadingPage() {
+  return <></>;
+}
