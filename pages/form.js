c2b05ed789e38cd52e42fa73a4b29cceaccae40a
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-08-14 15:22:41 -0700

    fixed form

--- pages/form.js
+++ pages/form.js
@@ -1,62 +1,65 @@
 import React from "react";
+import styles from "../styles/Form.module.css";
 import { useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
 import { supabase } from "../utils/supabaseClient.js";
 import { useState, useEffect } from "react";
 
-export default function Form() {
-  const { data: session, status } = useSession();
-  const { register, handleSubmit, errors, reset } = useForm();
+function Form() {
+  const { data: session } = useSession();
   const [username, setUsername] = useState();
 
   useEffect(() => {
     setUsername(session?.user?.username);
   }, [session]);
 
-  return status === "authenticated"
-    ? FormPage(username, register)
-    : LoadingPage();
-}
-
-function FormPage(username, register) {
-  const current_user = username;
-  console.log(current_user);
+  const { register, handleSubmit, errors, reset } = useForm();
+  const onSubmit = async (data) => {
+    const frm = document.getElementsByName("product-form")[0];
+    frm.reset();
+    data["user"] = username;
+    console.log(data);
+    await supabase.from("Items").insert([data]);
+  };
 
   return (
-    <form
-      onSubmit={async (data) => {
-        data["user"] = current_user;
-        console.log(data);
-        await supabase.from("Items").insert(data);
-      }}
-    >
-      <label htmlFor="name">Product Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        {...register("name", { required: true })}
-      />
-      <label htmlFor="price">Price</label>
-      <input
-        type="number"
-        id="price"
-        name="price"
-        step="0.01"
-        {...register("price", { required: true })}
-      />
-      <label htmlFor="quantity">Quantity</label>
-      <input
-        type="number"
-        id="quanity"
-        name="quantity"
-        {...register("quantity", { required: true })}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className={`${styles.formBorder}`}>
+      <div className="row justify-content-center my-5">
+        <div className="col-lg-8">
+          <form onSubmit={handleSubmit(onSubmit)} name="product-form">
+            <label for="name">Product Name</label>
+            <input
+              className="form-control mb-4"
+              type="text"
+              id="name"
+              name="name"
+              {...register("name", { required: true })}
+            />
+            <label for="price">Price</label>
+            <input
+              className="form-control mb-4"
+              type="number"
+              id="price"
+              name="price"
+              step="0.01"
+              {...register("price", { required: true })}
+            />
+            <label for="quantity">Quantity</label>
+            <input
+              className="form-control mb-4"
+              type="number"
+              id="quanity"
+              name="quantity"
+              {...register("quantity", { required: true })}
+            />
+            <div className="mb-4 text-center">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
-function LoadingPage() {
-  return <></>;
-}
+export default Form;
